9a3c3020fecf1a4a5b52193fc08ca6e89ae9e5ba
Abhijit Patil <abhiarunpatil@gmail.com>
2018-03-08 12:25:36 +0530

    Initial commit

---
+++ IrisPredictor.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+import numpy as np
+
+print(tf.__version__)
+
+from tensorflow.contrib.learn.python.learn.datasets import base
+
+# declare data files
+IRIS_TRAINING_DATA = "iris_training.csv"
+IRIS_TEST_DATA = "iris_test.csv"
+
+# load datasets from csv
+training_set = base.load_csv_with_header(filename=IRIS_TRAINING_DATA, features_dtype=np.float32, target_dtype=np.int)
+test_set = base.load_csv_with_header(filename=IRIS_TEST_DATA, features_dtype=np.float32, target_dtype=np.int)
+
+print(training_set.data)
+
+print(training_set.target)
+
+# Specify that all features have real-value data
+feature_name = "flower_features"
+feature_columns = [tf.feature_column.numeric_column(feature_name, shape=[4])]
+
+classifier = tf.estimator.LinearClassifier(feature_columns=feature_columns, n_classes=3, model_dir="/tmp/iris_model")
+
+def input_fn(dataset):
+    def _fn():
+        features = {feature_name: tf.constant(dataset.data)}
+        label = tf.constant(dataset.target)
+        return features, label
+    return _fn
+
+print(input_fn(training_set)())
+
+# fit model
+classifier.train(input_fn=input_fn(training_set), steps=1000)
+print("fit done")
+
+# evaluate accuracy
+accuracy_score = classifier.evaluate(input_fn=input_fn(test_set), steps=100)["accuracy"]
+print("\nAccuracy: {0:f}".format(accuracy_score))
